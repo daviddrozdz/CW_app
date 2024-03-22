ca17c1e3cd7d2ff6a911382e3f545ca4df4baaca
daviddrozdz <dawid.drozdz@students.iaac.net>
2024-03-22 08:58:00 +0100

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,13 +3,15 @@
   <div>
 
     <h1 class="absolute text-9xl font-medium text-blue-500 z-30 select-none">
-      Asseta app
+      Assets app
     </h1>
 
     <AssetPagination>
 
       <AssetGrid :assets="assets" v-slot="{ asset, index }">
+
         <NuxtLink :to="`/asset/${asset.id}`" @click.native="active = asset.id">
+          
           <AssetCard
             :class="[
               { active: active === asset.id },
@@ -17,7 +19,9 @@
               'hover:bg-blue-600 scale-90 hover:scale-95 rounded-md',
             ]"
           />
+          
         </NuxtLink>
+
       </AssetGrid>
 
     </AssetPagination>
@@ -32,17 +36,18 @@
 
 const active = useState<string>();
 
-// const assets = [
-//   { id: "1", project_id: "1", speckle_id: "1" },
-//   { id: "2", project_id: "2", speckle_id: "2" },
-//   { id: "3", project_id: "3", speckle_id: "3" },
-//   { id: "4", project_id: "4", speckle_id: "4" },
-//   { id: "5", project_id: "5", speckle_id: "5" },
-//   { id: "6", project_id: "6", speckle_id: "6" },
-//   { id: "7", project_id: "7", speckle_id: "7" },
-//   { id: "8", project_id: "8", speckle_id: "8" },
-//   { id: "9", project_id: "9", speckle_id: "9" },
-// ];
+// Speckle assets are here
+const assets = [
+  { id: "1", project_id: "1", speckle_id: "1" },
+  { id: "2", project_id: "2", speckle_id: "2" },
+  { id: "3", project_id: "3", speckle_id: "3" },
+  { id: "4", project_id: "4", speckle_id: "4" },
+  { id: "5", project_id: "5", speckle_id: "5" },
+  { id: "6", project_id: "6", speckle_id: "6" },
+  { id: "7", project_id: "7", speckle_id: "7" },
+  { id: "8", project_id: "8", speckle_id: "8" },
+  { id: "9", project_id: "9", speckle_id: "9" },
+];
 
 const { data: assets, pending, refresh } = await useFetch("/api/assets");
 watchEffect(() => {
