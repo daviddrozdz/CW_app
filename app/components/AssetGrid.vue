26143bb9e62296df6d0479f4f1371e2031f81ea2
daviddrozdz <dawid.drozdz@students.iaac.net>
2024-03-22 08:49:14 +0100

    Creating asset grid and asset card

---
+++ app/components/AssetGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="grid grid-cols-3 grid-rows-3 h-screen max-w-6xl">
+    <template v-for="(asset, index) of assets" :key="asset.id">
+      <slot v-bind="{ asset, index }"></slot>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  assets:
+    | {
+        id: string;
+        speckle_id: string | null;
+        project_id: string | null;
+      }[]
+    | null;
+}>();
+</script>
+
+<style scoped></style>
